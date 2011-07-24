5b70ab700b0b30ead08362a1b6fe54b8ad827bab
Jason Toffaletti <jason@topsy.com>
2011-07-24 02:24:56 -0700

    use shared_ptr for channels

--- channel.hh
+++ channel.hh
@@ -1,56 +1,81 @@
 #ifndef CHANNEL_HH
 #define CHANNEL_HH
 
+#include <boost/shared_ptr.hpp>
 #include <boost/circular_buffer.hpp>
 #include "thread.hh"
+#include "task.hh"
 
 // based on bounded_buffer example
 // http://www.boost.org/doc/libs/1_41_0/libs/circular_buffer/doc/circular_buffer.html#boundedbuffer
 
-//! send and receive pointers between tasks
-template <typename T> class channel : boost::noncopyable {
-public:
+namespace detail {
+template <typename T> struct impl : boost::noncopyable {
     typedef typename boost::circular_buffer<T> container_type;
     typedef typename container_type::size_type size_type;
 
-    //! default is unbuffered (send will block until recv)
-    channel(size_type capacity=0) : m_capacity(capacity), m_unread(0),
-        m_container(capacity ? capacity : 1) {}
+    impl(size_type capacity_=0) : capacity(capacity_), unread(0),
+        container(capacity ? capacity : 1) {}
 
-    size_type send(T p) {
-        mutex::scoped_lock l(m_mutex);
-        size_type unread = m_unread;
-        while (is_full()) {
-            m_not_full.wait(l);
+    // capacity is different than container.capacity()
+    // to avoid needing to reallocate the container
+    // on every send/recv for unbuffered channels
+    size_type capacity;
+    size_type unread;
+    container_type container;
+    mutex mtx;
+    // using task-aware conditions
+    task::condition not_empty;
+    task::condition not_full;
+
+    bool is_empty() const { return unread == 0; }
+    bool is_full() const { return unread >= capacity; }
+};
+
+} // end namespace detail
+
+//! send and receive pointers between tasks
+template <typename T> class channel {
+public:
+    //! default is unbuffered (send will block until recv)
+    channel(typename detail::impl<T>::size_type capacity=0) {
+       m.reset(new detail::impl<T>(capacity));
+    }
+
+    typename detail::impl<T>::size_type send(T p) {
+        mutex::scoped_lock l(m->mtx);
+        typename detail::impl<T>::size_type unread = m->unread;
+        while (m->is_full()) {
+            m->not_full.wait(l);
         }
-        m_container.push_front(p);
-        ++m_unread;
-        m_not_empty.signal();
+        m->container.push_front(p);
+        ++m->unread;
+        m->not_empty.signal();
         return unread;
     }
 
     T recv() {
         T item;
-        mutex::scoped_lock l(m_mutex);
-        bool unbuffered = m_capacity == 0;
+        mutex::scoped_lock l(m->mtx);
+        bool unbuffered = m->capacity == 0;
         if (unbuffered) {
             // grow the capacity for a single item
-            m_capacity = 1;
+            m->capacity = 1;
             // unblock sender
-            m_not_full.signal();
+            m->not_full.signal();
         }
-        while (is_empty()) {
-            m_not_empty.wait(l);
+        while (m->is_empty()) {
+            m->not_empty.wait(l);
         }
         // we don't pop_back because the item will just get overwritten
         // when the circular buffer wraps around
-        item = m_container[--m_unread];
+        item = m->container[--m->unread];
         if (unbuffered) {
             // shrink capacity again so sends will block
             // waiting for recv
-            m_capacity = 0;
+            m->capacity = 0;
         } else {
-            m_not_full.signal();
+            m->not_full.signal();
         }
         return item;
     }
@@ -60,23 +85,11 @@ public:
     }
 
     size_t unread() {
-        mutex::scoped_lock lock(m_mutex);
-        return m_unread;
+        mutex::scoped_lock lock(m->mtx);
+        return m->unread;
     }
 private:
-    // m_capacity is different than m_container.capacity()
-    // to avoid needing to reallocate the container
-    // on every send/recv for unbuffered channels
-    size_type m_capacity;
-    size_type m_unread;
-    container_type m_container;
-    mutex m_mutex;
-    // using task-aware conditions
-    task::condition m_not_empty;
-    task::condition m_not_full;
-
-    bool is_empty() const { return m_unread == 0; }
-    bool is_full() const { return m_unread >= m_capacity; }
+    boost::shared_ptr<detail::impl<T> > m;
 };
 
 #endif // CHANNEL_HH
--- prime_sieve.cc
+++ prime_sieve.cc
@@ -3,28 +3,28 @@
 
 #include <boost/bind.hpp>
 
-void generate(channel<int> *out) {
+void generate(channel<int> out) {
     for (int i=2; ; ++i) {
-        out->send(i);
+        out.send(i);
     }
 }
 
-void filter(channel<int> *in, channel<int> *out, int prime) {
+void filter(channel<int> in, channel<int> out, int prime) {
     for (;;) {
-        int i = in->recv();
+        int i = in.recv();
         if (i % prime != 0) {
-            out->send(i);
+            out.send(i);
         }
     }
 }
 
 void primes() {
-    channel<int> *ch = new channel<int>;
+    channel<int> ch;
     task::spawn(boost::bind(generate, ch));
     for (int i=0; i<100; ++i) {
-        int prime = ch->recv();
+        int prime = ch.recv();
         printf("%i\n", prime);
-        channel<int> *out = new channel<int>;
+        channel<int> out;
         task::spawn(boost::bind(filter, ch, out, prime));
         ch = out;
     }
