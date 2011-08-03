0de0d36f8f75d67a064f904bfce55a430ebe406d
Jason Toffaletti <jason@topsy.com>
2011-08-03 03:05:25 -0700

    reference counted bounds checked buffer/slices

---
+++ buffer.hh
@@ -0,0 +1,89 @@
+#ifndef BUFFER_HH
+#define BUFFER_HH
+
+//! reference counted buffer
+class buffer {
+private:
+    struct impl {
+        char *p;
+        size_t n;
+
+        impl(size_t bytes=0) : p(bytes ? new char[bytes] : 0), n(bytes) {}
+        ~impl() { delete[] p; }
+    };
+    typedef boost::shared_ptr<impl> shared_impl;
+
+    shared_impl m;
+
+public:
+    //! a pointer and length to a buffer's memory
+    class slice {
+    private:
+        friend class buffer;
+        //! hold a reference to the buffer this slice belongs to
+        shared_impl m;
+        char *p;
+        size_t len;
+
+        slice(const shared_impl &m_, size_t pos, size_t len_)
+            : m(m_), p(0), len(len_)
+        {
+            // bounds check
+            assert(m->p);
+            assert(pos+len <= m->n);
+            p = &m->p[pos];
+        }
+    public:
+
+        slice() : p(0), len(0) {}
+
+        //! create a slice from this slice
+        slice operator ()(size_t pos, size_t len) {
+            return slice(m, (p-m->p)+pos, len);
+        }
+
+        char &operator [](size_t pos) {
+            assert(p);
+            assert(pos < len);
+            return p[pos];
+        }
+
+        char &operator [](size_t pos) const {
+            assert(p);
+            assert(pos < len);
+            return p[pos];
+        }
+
+        //! dangerous, but useful cast to char *
+        operator char *() const {
+            assert(p);
+            return p;
+        }
+
+        size_t size() const { return len; }
+
+        size_t maxsize() const { return m->n - (p-m->p); }
+
+        void resize(size_t nlen) {
+            assert((p-m->p)+nlen <= m->n);
+            len=nlen;
+        }
+
+    };
+public:
+    //! allocate a block of memory
+    buffer(size_t bytes=0) : m(new impl(bytes)) {}
+
+    //! slice this buffer at pos
+    slice operator ()(size_t pos) const {
+        return slice(m, pos, m->n-pos);
+    }
+
+    //! slice at pos with len bytes
+    slice operator ()(size_t pos, size_t len) const {
+        return slice(m, pos, len);
+    }
+};
+
+#endif // BUFFER_HH
+
--- file-send.cc
+++ file-send.cc
@@ -1,6 +1,7 @@
 #include "runner.hh"
 #include "task.hh"
 #include "channel.hh"
+#include "buffer.hh"
 #include <boost/bind.hpp>
 
 // sends a file over a tcp socket on port 5500
@@ -9,53 +10,45 @@
 // 30faaa8f73d753d422ef3d0646a38297  out
 // 30faaa8f73d753d422ef3d0646a38297  libfw.a
 
-struct buffer {
-    char *buf;
-    size_t len;
-};
-
 #define channel_cap 0
-// need two buffers more than the channel cap
+// need two 4096 byte blocks more than the channel cap
 // because we'll be writing into the next one while
-// file_sender is sending from one
+// file_sender is sending from the one it recv'ed from the channel
 #define buffer_cap channel_cap+2
 
-void file_reader(channel<buffer> c, file_fd &f) {
-    // use two buffers because we'll hand one off to file_sender
-    // while we read into the next one
-    buffer bufs[buffer_cap];
-    // allocate buffers on the heap so they will still
-    // exist for file_sender after this task has exited
-    for (int i=0; i<buffer_cap; ++i) {
-        bufs[i].buf = new char[4096];
-    }
+void file_reader(channel<buffer::slice> c, file_fd &f) {
+    // circular buffer for reading/writing blocks
+    // buffer is reference counted so it will be freed once
+    // the channel is freed and all references to slices are removed
+    buffer buf(buffer_cap*4096);
     int bufp = 0;
     for (;;) {
+        // create a 4096 byte slice of the buffer
+        buffer::slice bs = buf(bufp*4096, 4096);
         // read block from file
-        ssize_t nr = f.read(bufs[bufp].buf, 4096);
+        ssize_t nr = f.read(bs, bs.size());
         if (nr <= 0) break;
-        // send block on channel
-        bufs[bufp].len = nr;
-        c.send(bufs[bufp]);
-        // use the other buffer while file_sender is
-        // busy with the one we just sent
+        bs.resize(nr);
+        // send block on channel to file_sender task
+        c.send(bs);
+        // move positions in the buffer to the next 4096 byte block
         bufp++;
         if (bufp == buffer_cap) bufp = 0;
     }
-    buffer empty = {0,0};
+    buffer::slice empty;
     c.send(empty);
     c.close();
 }
 
-void file_sender(channel<buffer> c) {
+void file_sender(channel<buffer::slice> c) {
     address addr("127.0.0.1", 5500);
     task::socket s(AF_INET, SOCK_STREAM);
     s.connect(addr, 100);
     for (;;) {
-        buffer buf = c.recv();
-        if (buf.len == 0) break;
-        ssize_t nw = s.send(buf.buf, buf.len);
-        assert(nw == buf.len);
+        buffer::slice bs = c.recv();
+        if (bs.size() == 0) break;
+        ssize_t nw = s.send(bs, bs.size());
+        assert(nw == bs.size());
     }
 }
 
@@ -63,7 +56,7 @@ int main(int argc, char *argv[]) {
     if (argc < 2) return 1;
     runner::init();
     file_fd f(argv[1], 0, 0);
-    channel<buffer> c(channel_cap);
+    channel<buffer::slice> c(channel_cap);
     task::spawn(boost::bind(file_sender, c));
     // start a new OS-thread for file_reader
     runner::spawn(boost::bind(file_reader, c, boost::ref(f)));
