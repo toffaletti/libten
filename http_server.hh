6e0359bd9317cc79f5fb0c8579ab0d6e170aa31d
Jason Toffaletti <toffaletti@gmail.com>
2011-10-27 03:27:47 -0700

    use monotonic timer

--- http_server.hh
+++ http_server.hh
@@ -19,7 +19,7 @@ public:
     struct request {
         request(http_request &req_, netsock &sock_)
             : req(req_), resp(404, "Not Found"), sock(sock_),
-            start(std::chrono::high_resolution_clock::now()),
+            start(std::chrono::monotonic_clock::now()),
             resp_sent(false) {}
 
         //! compose a uri from the request uri
