4a925b49ab7d86263c5ce803b11add137ee5991f
Jason Toffaletti <jason@topsy.com>
2011-10-28 18:32:46 -0700

    fix include for glog

--- cmake/ten.cmake
+++ cmake/ten.cmake
@@ -7,10 +7,10 @@ add_definitions(-DUSE_BOOST_FCONTEXT)
 include_directories(${CWD}/..) # for boost.context
 include_directories(${CWD}/../jansson)
 include_directories(${CWD}/../msgpack)
+include_directories(${CWD}/../glog)
 
 file(RELATIVE_PATH REL ${CMAKE_CURRENT_SOURCE_DIR} ${CWD}/..)
-get_filename_component(LIBTEN_INC ${CMAKE_CURRENT_BINARY_DIR}/${REL}/include ABSOLUTE)
-get_filename_component(LIBTEN_LIB ${CMAKE_CURRENT_BINARY_DIR}/${REL}/lib ABSOLUTE)
-
-include_directories(${LIBTEN_INC})
-link_directories(${LIBTEN_LIB})
+#get_filename_component(LIBTEN_INC ${CMAKE_CURRENT_BINARY_DIR}/${REL}/include ABSOLUTE)
+#get_filename_component(LIBTEN_LIB ${CMAKE_CURRENT_BINARY_DIR}/${REL}/lib ABSOLUTE)
+#include_directories(${LIBTEN_INC})
+#link_directories(${LIBTEN_LIB})
