6ef4290d2dc06f3a0983e5f74321884b30c419d8
Jason Toffaletti <jason@topsy.com>
2012-02-21 19:09:48 -0800

    add generic class for doing backoff timing

---
+++ tests/test_backoff.cc
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE backoff test
+#include <boost/test/unit_test.hpp>
+#include "backoff.hh"
+
+using namespace ten;
+using namespace std::chrono;
+
+BOOST_AUTO_TEST_CASE(backoff_test) {
+    backoff<seconds> b(seconds(1), seconds(60));
+    for (auto i=0; i<1000; ++i) {
+        seconds delay = b.next_delay();
+        BOOST_REQUIRE(delay.count() >= 1);
+        BOOST_REQUIRE(delay.count() <= 60);
+    }
+}
+
