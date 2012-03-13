7aca96536d0fd1031d3e2acdad4e01fd25582eb2
Jason Toffaletti <toffaletti@gmail.com>
2012-03-13 08:31:33 -0700

    more work in progress speedtracer support

--- speedtracer.hh
+++ speedtracer.hh
@@ -1,9 +1,14 @@
 #include <chrono>
 #include <deque>
+#include <vector>
 #include <string>
 #include <ostream>
 #include "json.hh"
 #include "logging.hh"
+
+// TODO: rework this to have a trace and event classes
+// trace holds the list of events
+// 
 
 // http://code.google.com/webtoolkit/speedtracer/data-dump-format.html
 // http://code.google.com/p/google-web-toolkit/source/browse/trunk/dev/core/src/com/google/gwt/dev/util/log/speedtracer/SpeedTracerLogger.java
@@ -20,219 +25,54 @@ using namespace std::chrono;
     __PRETTY_FUNCTION__ \
 
 namespace tracer {
-
 typedef high_resolution_clock clock;
 
-struct record {
-    uint64_t id;
+struct event;
+
+struct session {
     clock::time_point start;
-    clock::time_point end;
+    std::vector<event> children;
 
-    record(uint64_t id_) : id(id_), start(move(clock::now())) {}
+    session() : start(clock::now()) {}
 
-    bool finished() const {
-        return end.time_since_epoch().count() != 0;
-    }
-
-    void finish() {
-        if (finished()) return;
-        end = clock::now();
-    }
-
-    clock::duration duration() const {
-        if (finished()) {
-            return end - start;
-        } else {
-            return clock::now() - start;
-        }
-    }
-
-    json range() {
-        json r(json::object());
-        r.oset("duration", (json_int_t)duration_cast<milliseconds>(duration()).count());
-        json s(json::array());
-        uint64_t ms = duration_cast<milliseconds>(start.time_since_epoch()).count();
-        s.apush((json_int_t)ms / 1000);
-        s.apush((json_int_t)ms % 1000);
-        r.oset("start", s);
-
-        json e(json::array());
-        ms = duration_cast<milliseconds>(end.time_since_epoch()).count();
-        e.apush((json_int_t)ms / 1000);
-        e.apush((json_int_t)ms % 1000);
-        r.oset("end", e);
-
-        return r;
-    }
+    json to_json() const;
 };
 
-struct event : record {
-    string label;
-    const char *file;
-    uint32_t line;
-    const char *method;
-    std::deque<event> children;
+struct event {
+    shared_ptr<session> s;
+    int32_t type;
+    clock::time_point start;
+    std::vector<event> children;
+    json data;
 
-    //! must use string literals for these
-    event(uint64_t id,
-            string label_,
-            const char *file_=0,
-            uint32_t line_=0,
-            const char *method_=0
-         )
-        : record(id), label(move(label_)), file(file_), line(line_), method(method_)
+    event(shared_ptr<session> s_, int32_t type_=-2)
+        : s(s_), type(type_), start(clock::now()), data(json::object())
     {
     }
 
-    json to_json() {
+    //! milliseconds since start of session
+    milliseconds time() const {
+        return duration_cast<milliseconds>(start - s->start);
+    }
+
+    json to_json() const {
         json e(json::object());
-        e.oset("range", range());
-        e.oset("id", (json_int_t)id);
-
-        json op(json::object());
-        op.oset("type", "METHOD");
-        op.oset("label", label);
-        
-        json scl(json::object());
-        string fn(file);
-        scl.oset("className", fn.substr(fn.rfind("/")+1));
-        scl.oset("methodName", method);
-        scl.oset("lineNumber", (json_int_t)line);
-        op.oset("sourceCodeLocation", scl);
-
-        e.oset("operation", op);
-
-        json ch(json::array());
-        for (auto i=children.begin(); i!=children.end(); ++i) {
-            ch.apush(i->to_json());
-        }
-        e.oset("children", ch);
+        e.oset("type", (json_int_t)type);
+        e.oset("time", (json_int_t)time().count());
+        e.oset("data", data);
         return e;
     }
-
-    friend ostream &operator <<(ostream &o, const event &e) {
-        using ::operator<<;
-        o << e.label << "[" << e.method << "@" << e.file << ":" << e.line << "] " << 
-            duration_cast<milliseconds>(e.duration()).count() << "ms\n" << e.children;
-        return o;
-    }
 };
 
-struct tracer : event {
-    uint64_t event_id;
-    std::deque<event *> call_stack;
 
-    tracer(uint64_t id, string label) : event(id, label), event_id(0) {
-        call_stack.push_back(this);
+json session::to_json() const {
+    json a(json::array());
+    for (auto i=children.begin(); i!=children.end(); ++i) {
+        a.apush(i->to_json());
     }
-
-    uint64_t next_id() {
-        return ++event_id;
-    }
-
-    // TODO: allow concurrent traces:
-    // t1
-    // \-- t2
-    // \-------t3
-    // \-----t4
-    uint64_t trace_start(
-        string label_,
-        const char *file_,
-        uint32_t line_,
-        const char *method_,
-        bool next_level
-        )
-    {
-        event *parent = call_stack.back();
-        parent->children.push_back(
-                event(next_id(),
-                    move(label_),
-                    file_,
-                    line_,
-                    method_)
-                );
-        if (next_level) {
-            call_stack.push_back(&parent->children.back());
-        }
-        return parent->children.back().id;
-    }
-
-    void trace_end(uint64_t id) {
-        event *e = call_stack.back();
-        if (e->id == id) {
-            // TODO: finish should check children
-            // to make sure they are all finished
-            e->finish();
-            call_stack.pop_back();
-            return;
-        } else {
-            // TODO: this would need to walk the call stack
-            // for more advanced concurrency models
-            for (auto i = e->children.begin(); i != e->children.end(); ++i) {
-                if (i->id == id) {
-                    i->finish();
-                    return;
-                }
-            }
-        }
-        LOG(FATAL) << "bad call stack";
-    }
-
-    json to_json() {
-        json trace(json::object());
-        trace.oset("date", (json_int_t)duration_cast<seconds>(start.time_since_epoch()).count());
-        trace.oset("application", "libten");
-        trace.oset("range", range());
-        trace.oset("id", (json_int_t)id);
-
-        json frame(json::object());
-        frame.oset("range", range());
-        frame.oset("id", (json_int_t)id);
-
-        json op(json::object());
-        op.oset("type", "HTTP");
-        op.oset("label", label);
-        frame.oset("operation", op);
-
-        trace.oset("frameStack", frame);
-
-        json ch(json::array());
-        for (auto i=children.begin(); i!=children.end(); ++i) {
-            ch.apush(i->to_json());
-        }
-        trace.oset("children", ch);
-
-        json root(json::object());
-        root.oset("trace", trace);
-        return root;
-    }
-
-    friend ostream &operator <<(ostream &o, const tracer &t) {
-        using ::operator<<;
-        o << t.label << " " << duration_cast<milliseconds>(t.duration()).count() << "ms\n" << t.children;
-        return o;
-    }
-};
-
-struct trace {
-    tracer &tr;
-    uint64_t id;
-
-    trace(tracer &tr_,
-            string label_,
-            const char *file_,
-            uint32_t line_,
-            const char *method_,
-            bool next_level = true
-         ) : tr(tr_), id(0)
-    {
-        id = tr.trace_start(label_, file_, line_, method_, next_level);
-    }
-
-    ~trace() {
-        tr.trace_end(id);
-    }
-};
+    return a;
+}
 
 } // end namespace tracer
+
 } // end namespace ten
--- tests/test_speedtracer.cc
+++ tests/test_speedtracer.cc
@@ -8,31 +8,34 @@ using namespace std;
 
 const size_t default_stacksize=256*1024;
 
-static void do_request(tracer::tracer &tr, channel<int> ch, string resource) {
-    tracer::trace t(tr, resource, TRACE, false);
+#if 0
+static void do_request(tracer::trace &tr, channel<int> ch, string resource) {
+    tracer::trace t(tr, resource, TRACE);
     tasksleep(rand() % 100);
     ch.send(0);
 }
 
-static void do_all(tracer::tracer &tr) {
+static void do_all(tracer::trace &tr) {
     tracer::trace t(tr, "do all", TRACE);
     channel<int> ch;
-    taskspawn(bind(do_request, ref(tr), ch, "a"));
-    taskspawn(bind(do_request, ref(tr), ch, "b"));
-    taskspawn(bind(do_request, ref(tr), ch, "c"));
+    taskspawn(bind(do_request, ref(t), ch, "a"));
+    taskspawn(bind(do_request, ref(t), ch, "b"));
+    taskspawn(bind(do_request, ref(t), ch, "c"));
     ch.recv();
     ch.recv();
     ch.recv();
 }
+#endif
 
 // TODO: flesh this out
 BOOST_AUTO_TEST_CASE(task_socket_dial) {
-    tracer::tracer tr(0, "test tracer");
-    procmain p;
-    taskspawn(bind(do_all, ref(tr)));
-    p.main();
-    tr.finish();
-    LOG(INFO) << tr;
-    LOG(INFO) << tr.to_json().dump(2);
+    //tracer::tracer tr(0, "test tracer");
+    //tracer::trace t(tr);
+    //procmain p;
+    //taskspawn(bind(do_all, ref(t)));
+    //p.main();
+    //tr.finish();
+    //LOG(INFO) << tr;
+    //LOG(INFO) << tr.to_json().dump(2);
 }
 
