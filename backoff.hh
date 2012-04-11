6dd3378d677c70cc97f16771886163a6a7894023
Jason Toffaletti <jason@topsy.com>
2012-04-11 16:50:05 -0700

    coding convention

--- backoff.hh
+++ backoff.hh
@@ -7,26 +7,26 @@ namespace ten {
 
 template <typename DurationT> class backoff {
 private:
-    std::minstd_rand eng;
-    std::tr1::variate_generator<std::minstd_rand, std::tr1::uniform_real<float>> rand;
-    DurationT min_delay;
-    DurationT max_delay;
-    uint64_t retry;
+    std::minstd_rand _eng;
+    std::tr1::variate_generator<std::minstd_rand, std::tr1::uniform_real<float>> _rand;
+    DurationT _min_delay;
+    DurationT _max_delay;
+    uint64_t _retry;
 public:
-    backoff(const DurationT &min_delay_, const DurationT &max_delay_)
-        : eng(time(0)), rand(eng, std::tr1::uniform_real<float>(0.0, 1.0)),
-        min_delay(min_delay_), max_delay(max_delay_), retry(0)
+    backoff(const DurationT &min_delay, const DurationT &max_delay)
+        : _eng(time(0)), _rand(_eng, std::tr1::uniform_real<float>(0.0, 1.0)),
+        _min_delay(min_delay), _max_delay(max_delay), _retry(0)
     {
     }
 
-    void reset() { retry = 0; }
+    void reset() { _retry = 0; }
 
     DurationT next_delay()
     {
         double rnd = rand();
-        typename DurationT::rep tmp = min_delay.count() + (min_delay.count() * rnd * (retry + 1));
-        typename DurationT::rep rep = std::min(max_delay.count(), tmp);
-        ++retry;
+        typename DurationT::rep tmp = _min_delay.count() + (_min_delay.count() * rnd * (_retry + 1));
+        typename DurationT::rep rep = std::min(_max_delay.count(), tmp);
+        ++_retry;
         return DurationT(rep);
     }
 };
