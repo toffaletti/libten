f5499ed2ae9ac26f8b7e4229114b6c31d6821e58
Chip Salzenberg <chip@pobox.com>
2012-04-20 02:45:32 -0700

    introduce Microsoft's SafeInt<> (pending rename) and jserial support for it

---
+++ include/ten/jserial_safeint.hh
@@ -0,0 +1,27 @@
+#ifndef LIBTEN_JSERIAL_SAFEINT_HH
+#define LIBTEN_JSERIAL_SAFEINT_HH
+
+#include "ten/jserial.hh"
+#include "ten/SafeInt3.hpp"
+#include <type_traits>
+
+namespace ten {
+using std::move;
+
+template <class I>
+struct stor_traits<SafeInt<I>> : public json_traits_conv<SafeInt<I>> {
+    static SafeInt<I> cast(const json &j)          { return json_cast<I>(j); }
+};
+template <class I>
+inline json to_json(SafeInt<I> i)             { return to_json(i.Ref()); }
+
+
+template <class AR, class I, class X = typename std::enable_if<AR::is_archive>::type>>
+inline AR & operator & (AR &ar, SafeInt<I> &si) {
+    ar & si.Ref();
+    return ar;
+}
+
+} // ten
+
+#endif LIBTEN_JSERIAL_SAFEINT_HH
