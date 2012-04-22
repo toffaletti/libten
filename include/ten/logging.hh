379f9ed71e9483b6208417b797c59970e703e8f8
Jason Toffaletti <toffaletti@gmail.com>
2012-04-22 00:05:04 -0700

    remove to_hex from logging.hh

--- include/ten/logging.hh
+++ include/ten/logging.hh
@@ -3,21 +3,5 @@
 
 #include <glog/logging.h>
 #include <glog/stl_logging.h>
-#include <sstream>
-
-namespace ten {
-
-//! hexify memory
-template <typename T> std::string to_hex(T &in) {
-  static const char hexs[] = "0123456789ABCDEF";
-  std::stringstream ss;
-  for (typename T::const_iterator i = in.begin(); i!=in.end(); i++) {
-    ss << hexs[(*i>>4) & 0xF];
-    ss << hexs[*i & 0x0F];
-  }
-  return ss.str();
-}
-
-} // end namespace ten
 
 #endif // LIBTEN_LOGGING_HH
