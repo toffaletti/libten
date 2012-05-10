0598d9a05e75618d6df4af6641c24a185d0fbc51
Jason Toffaletti <jason@topsy.com>
2012-05-10 16:09:00 -0700

    add consistent hash ring implementation and tests

---
+++ include/ten/consistent_hash.hh
@@ -0,0 +1,57 @@
+#ifndef LIBTEN_CONSISTENT_HASH_HH
+#define LIBTEN_CONSISTENT_HASH_HH
+#include <map>
+#include <functional>
+#include <stdexcept>
+#include <boost/lexical_cast.hpp>
+
+namespace ten {
+
+class hash_ring {
+private:
+    typedef std::map<size_t, std::string> map_type;
+
+    unsigned _replicas;
+    map_type _ring;
+    std::hash<std::string> _hash;
+public:
+    hash_ring(unsigned replicas) : _replicas(replicas) {}
+
+    void add(const std::string &node, unsigned replicas=0) {
+        if (replicas == 0) replicas = _replicas;
+        size_t hash;
+        for (unsigned r = 0; r < replicas; ++r) {
+            hash = _hash(node + boost::lexical_cast<std::string>(r));
+            _ring[hash] = node;
+        }
+    }
+
+    unsigned erase(const std::string &node, unsigned replicas=0) {
+        if (replicas == 0) replicas = _replicas;
+        size_t hash;
+        unsigned found = 0;
+        for (unsigned r = 0; r < replicas; ++r) {
+            hash = _hash(node + boost::lexical_cast<std::string>(r));
+            found += _ring.erase(hash);
+        }
+        // return number of replicas erased
+        return found;
+    }
+
+    const std::string &get(const std::string &data) const {
+        if (_ring.empty()) {
+            throw std::runtime_error("empty hash_ring");
+        }
+        size_t hash = _hash(data);
+        // find first node >= hash
+        auto it = _ring.lower_bound(hash);
+        if (it == _ring.end()) {
+            // wrap around to the front
+            it = _ring.begin();
+        }
+        return it->second;
+    }
+};
+
+} // ns ten
+#endif
---
+++ tests/test_hash_ring.cc
@@ -0,0 +1,79 @@
+#define BOOST_TEST_MODULE hash_ring test
+#include <unordered_map>
+#include <boost/test/unit_test.hpp>
+#include "ten/consistent_hash.hh"
+
+using namespace ten;
+
+struct server {
+    std::unordered_map<std::string, std::string> cache;
+
+    void put(const std::string &key, const std::string &value) {
+        cache[key] = value;
+    }
+
+    std::string get(const std::string &key) const {
+        std::string value;
+        auto it = cache.find(key);
+        if (it != cache.end()) {
+            value = it->second;
+        }
+        return value;
+    }
+};
+
+static const std::map<std::string, std::string> data = {
+    {"zero",  "0"},
+    {"one",   "1"},
+    {"two",   "2"},
+    {"three", "3"},
+    {"four",  "4"},
+    {"five",  "5"},
+    {"six",   "6"},
+    {"seven", "7"},
+    {"eight", "8"},
+    {"nine",  "9"},
+    {"ten",   "10"},
+};
+
+BOOST_AUTO_TEST_CASE(hash_ring_basic_test) {
+    std::unordered_map<std::string, server> servers = {
+        {"server1.example.com", server()},
+        {"server2.example.com", server()},
+        {"server3.example.com", server()},
+    };
+
+    hash_ring ring(1);
+    for (auto it : servers) {
+        ring.add(it.first);
+    }
+
+    for (auto it : data) {
+        auto host = ring.get(it.first);
+        BOOST_MESSAGE("storing " << it.first << "=" << it.second << " on " << host);
+        servers[host].put(it.first, it.second);
+    }
+
+    for (auto it : data) {
+        auto host = ring.get(it.first);
+        auto data = servers[host].get(it.first);
+        BOOST_REQUIRE_EQUAL(it.second, data);
+    }
+
+    // remove a server
+    BOOST_REQUIRE_EQUAL(1, ring.erase("server3.example.com"));
+
+    unsigned found = 0;
+    for (auto it : data) {
+        auto host = ring.get(it.first);
+        auto data = servers[host].get(it.first);
+        if (it.second == data) {
+            ++found;
+        }
+    }
+    // more than 30% of the keys should still be found
+    // after removing one server
+    BOOST_REQUIRE(found / (float)data.size() > 0.30);
+}
+
+
