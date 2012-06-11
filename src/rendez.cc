4360017abb39ae9f87af5374c50124d4d1c07d04
Jason Toffaletti <jason@topsy.com>
2012-06-11 14:12:15 -0700

    optimize locking

--- src/rendez.cc
+++ src/rendez.cc
@@ -9,14 +9,10 @@ void rendez::sleep(std::unique_lock<qutex> &lk) {
 
     {
         std::unique_lock<std::timed_mutex> ll(_m);
-        //DCHECK(std::find(_waiting.begin(), _waiting.end(), t) == _waiting.end())
-        //    << "BUG: " << t << " already waiting on rendez " << this;
-        // TODO: investigate why this happens. im thinking spurious wakeups
-        // caused by some error in my lock-freeish event loop changes
-        if (std::find(_waiting.begin(), _waiting.end(), t) == _waiting.end()) {
-            DVLOG(5) << "RENDEZ " << this << " PUSH BACK: " << t;
-            _waiting.push_back(t);
-        }
+        DCHECK(std::find(_waiting.begin(), _waiting.end(), t) == _waiting.end())
+            << "BUG: " << t << " already waiting on rendez " << this;
+        DVLOG(5) << "RENDEZ[" << this << "] PUSH BACK: " << t;
+        _waiting.push_back(t);
     }
     // must hold the lock until we're in the waiting list
     // otherwise another thread might modify the condition and
@@ -26,10 +22,12 @@ void rendez::sleep(std::unique_lock<qutex> &lk) {
         t->swap(); 
         lk.lock();
     } catch (...) {
-        std::unique_lock<std::timed_mutex> ll(_m);
-        auto i = std::find(_waiting.begin(), _waiting.end(), t);
-        if (i != _waiting.end()) {
-            _waiting.erase(i);
+        {
+            std::unique_lock<std::timed_mutex> ll(_m);
+            auto i = std::find(_waiting.begin(), _waiting.end(), t);
+            if (i != _waiting.end()) {
+                _waiting.erase(i);
+            }
         }
         lk.lock();
         throw;
@@ -37,21 +35,28 @@ void rendez::sleep(std::unique_lock<qutex> &lk) {
 }
 
 void rendez::wakeup() {
-    std::unique_lock<std::timed_mutex> lk(_m);
-    if (!_waiting.empty()) {
-        task *t = _waiting.front();
-        _waiting.pop_front();
-        DVLOG(5) << "RENDEZ " << this << " wakeup: " << t;
-        t->ready();
+    task *t = nullptr;
+    {
+        std::unique_lock<std::timed_mutex> lk(_m);
+        if (!_waiting.empty()) {
+            t = _waiting.front();
+            _waiting.pop_front();
+        }
     }
+
+    DVLOG(5) << "RENDEZ[" << this << "] " << this_proc()->ctask << " wakeup: " << t;
+    if (t) t->ready();
 }
 
 void rendez::wakeupall() {
-    std::unique_lock<std::timed_mutex> lk(_m);
-    while (!_waiting.empty()) {
-        task *t = _waiting.front();
-        _waiting.pop_front();
-        DVLOG(5) << "RENDEZ " << this << " wakeupall: " << t;
+    tasklist waiting;
+    {
+        std::unique_lock<std::timed_mutex> lk(_m);
+        std::swap(waiting, _waiting);
+    }
+
+    for (auto t : waiting) {
+        DVLOG(5) << "RENDEZ[" << this << "] " << this_proc()->ctask << " wakeupall: " << t;
         t->ready();
     }
 }
