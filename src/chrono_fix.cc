01edb3d85677a24ae9aa03c4645ff371b1660a72
Jason Toffaletti <jason@topsy.com>
2012-11-27 17:17:55 -0800

    fix typo

--- src/chrono_fix.cc
+++ src/chrono_fix.cc
@@ -7,7 +7,7 @@ namespace std {
             timespec ts{};
             ::clock_gettime(CLOCK_MONOTONIC, &ts);
             return time_point(duration(
-                        static_cast<system_clock::rep>( ts.tv_sec ) * 1000000000 + ts.tv_nsec));
+                        static_cast<steady_clock::rep>( ts.tv_sec ) * 1000000000 + ts.tv_nsec));
         }
     }
 }
