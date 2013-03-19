f1532eda36eaf9d8ba777e4cdce233e096234baa
Jason Toffaletti <toffaleti@gmail.com>
2013-03-18 22:15:50 -0700

    don't use std::bind in backoff

--- include/ten/backoff.hh
+++ include/ten/backoff.hh
@@ -2,9 +2,7 @@
 #define LIBTEN_BACKOFF_HH
 
 #include "ten/error.hh"
-#include "ten/logging.hh" // temp
 #include <chrono>
-#include <algorithm>
 #include <random>
 
 namespace ten {
@@ -17,15 +15,15 @@ private:
     const DurationT _max_delay;
     uint64_t _try;
     std::minstd_rand _eng;
-    std::function<float ()> _rand;
+    float _scale;
 
 public:
     backoff(DurationT min_delay, DurationT max_delay, float scale = 1.0f)
         : _min_delay(min_delay),
           _max_delay(max_delay),
           _try(0),
-          _eng(time(nullptr)),
-          _rand(std::bind(std::uniform_real_distribution<float>(0.0f, scale), std::ref(_eng)))
+          _eng((long unsigned int)time(nullptr)),
+          _scale(scale)
     {
         if (min_delay.count() < 0 || min_delay > max_delay)
             throw errorx("invalid backoff(%jd, %jd)", intmax_t(min_delay.count()), intmax_t(max_delay.count()));
@@ -34,8 +32,9 @@ public:
     void reset() { _try = 0; }
 
     DurationT next_delay() {
+        std::uniform_real_distribution<float> rand_real{0.0f, _scale};
         ++_try;
-        float r = _rand();
+        float r = rand_real(_eng);
         float delta = 1.0f + (r * float(_try));
         const auto d = std::chrono::duration_cast<DurationT>(_min_delay * delta);
         return std::min(d, _max_delay);
