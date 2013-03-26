c9a05b05c009cebd8e656b6f6dfab063e81f0a31
Chip Salzenberg <chip@pobox.com>
2013-03-25 18:47:22 -0700

    use chrono_io and make_backoff() in backoff test

--- tests/test_backoff.cc
+++ tests/test_backoff.cc
@@ -1,12 +1,13 @@
 #define BOOST_TEST_MODULE backoff test
 #include <boost/test/unit_test.hpp>
 #include "ten/backoff.hh"
+#include <chrono_io>
 
 using namespace ten;
 using namespace std::chrono;
 
 BOOST_AUTO_TEST_CASE(backoff_test) {
-    backoff<seconds> b{seconds{1}, seconds{60}};
+    auto b = make_backoff(seconds{1}, seconds{60});
     for (auto i=0; i<1000; ++i) {
         seconds delay = b.next_delay();
         BOOST_REQUIRE(delay.count() >= 1);
@@ -15,12 +16,11 @@ BOOST_AUTO_TEST_CASE(backoff_test) {
 }
 
 BOOST_AUTO_TEST_CASE(backoff_test_min) {
-    backoff<seconds> b{seconds{1}, seconds{60}, 2};
-
+    auto b = make_backoff(milliseconds{1500}, seconds{60}, 2);
     for (int x = 0; x < 10; ++x) {
-        seconds delay = b.next_delay();
-        BOOST_MESSAGE("delay=" << delay.count());
-        BOOST_REQUIRE(delay.count() >= 1 && delay.count() < 60);
+        auto delay = b.next_delay();
+        BOOST_MESSAGE("delay = " << delay);
+        BOOST_REQUIRE(delay >= milliseconds{1500} && delay < seconds{60});
     }
 }
 
