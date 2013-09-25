aabc2f07d5a1686e0bb42c1b08176e79ad3bc229
Jason Toffaletti <jason@topsy.com>
2013-09-25 14:08:37 -0700

    add multi-get support to striped

--- include/ten/striped.hh
+++ include/ten/striped.hh
@@ -3,15 +3,42 @@
 
 #include <functional>
 #include <vector>
+#include <algorithm>
 
 namespace ten {
 
 template <class T> class striped {
+public:
+    typedef typename std::vector<T>::size_type size_type;
 private:
     std::vector<T> _stripes;
 
+    template <class Key, class... Keys>
+        std::vector<size_type> _mget(std::vector<size_type> &indexes, const Key key, const Keys... keys)
+    {
+        std::hash<Key> hasher;
+        size_t hash = hasher(key);
+        indexes.push_back(hash % _stripes.size());
+        return _mget(indexes, keys...);
+    }
+
+    std::vector<size_type> _mget(std::vector<size_type> &indexes) {
+        std::sort(begin(indexes), end(indexes));
+        return indexes;
+    }
 public:
-    striped(size_t size) : _stripes{size} {}
+    striped(size_type size) : _stripes(size) {}
+
+    template <class Key, class... Keys>
+        std::vector<size_type> multi_get(const Key key, const Keys... keys)
+   {
+        std::vector<size_type> indexes;
+        std::hash<Key> hasher;
+        size_t hash = hasher(key);
+        indexes.push_back(hash % _stripes.size());
+        return _mget(indexes, keys...);
+    }
+
 
     template <class Key> T &get(const Key &key) {
         std::hash<Key> hasher;
@@ -19,11 +46,11 @@ public:
         return at(hash % _stripes.size());
     }
 
-    T &at(const size_t index) {
+    T &at(const size_type index) {
         return _stripes.at(index);
     }
     
-    size_t size() const {
+    size_type size() const {
         return _stripes.size();
     }
 };
--- tests/test_striped.cc
+++ tests/test_striped.cc
@@ -4,10 +4,27 @@
 
 using namespace ten;
 
-TEST(Striped, Test) {
+TEST(StripedLock, Test) {
     striped<std::mutex> striped_lock{10};
 
     for (int i=0; i<100; ++i) {
         std::unique_lock<std::mutex> lock{striped_lock.get(i)};
     }
+}
+
+TEST(StripedMultiGet, Test) {
+    striped<int> striped_int{10};
+
+    {
+        std::vector<size_t> r1 = striped_int.multi_get(1, 2, 3, 4);
+        std::vector<size_t> r2 = striped_int.multi_get(4, 3, 1, 2);
+        EXPECT_EQ(r1, r2);
+    }
+
+    {
+        std::vector<size_t> r1 = striped_int.multi_get(1, 21, 305, 49);
+        std::vector<size_t> r2 = striped_int.multi_get(49, 305, 1, 21);
+        EXPECT_EQ(r1, r2);
+    }
+
 }
