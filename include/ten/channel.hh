ed8b55eaca423e42160f78a6fd8727086a05ede0
Jason Toffaletti <jason@topsy.com>
2013-11-07 16:30:07 -0800

    simplify channel logic. fix race condition in unbuffered mode.

--- include/ten/channel.hh
+++ include/ten/channel.hh
@@ -19,7 +19,7 @@ struct channel_closed_error : std::exception {
 //! send and receive data between tasks in FIFO order
 //
 //! channels can be buffered or unbuffered.
-//! unbuffered channels block on every send until recv
+//! unbuffered (synchronous) channels block on every send until recv
 //! buffered channels only block send when the buffer is full
 //! data is copied, so if you need to send large data
 //! its best to allocate them on the heap and send the pointer
@@ -32,8 +32,8 @@ private:
     bool _autoclose;
 public:
     //! create a new channel
-    //! \param capacity number of items to buffer. the default is 0, unbuffered.
-    explicit channel(typename impl::size_type capacity=0, bool autoclose=false)
+    //! \param capacity number of items to buffer. the default is 1, synchronous mode
+    explicit channel(typename impl::size_type capacity=1, bool autoclose=false)
         : _m(std::make_shared<impl>(capacity)), _autoclose(autoclose)
     {
     }
@@ -68,7 +68,7 @@ public:
     //! \return number of unread items
     size_t unread() {
         std::lock_guard<qutex> lock(_m->qtx);
-        return _m->unread;
+        return _m->queue.size();
     }
 
     bool is_closed() {
@@ -86,60 +86,61 @@ public:
 private:
     struct impl {
         typedef typename ContainerT::size_type size_type;
-        impl(size_type capacity_=0) : capacity(capacity_), unread(0),
-            queue(ContainerT()), closed(false) {}
+        impl(size_type capacity_=1) :
+            capacity(capacity_),
+            queue(ContainerT()),
+            closed(false)
+        {
+            CHECK(capacity_ >= 1) << "channel capacity must be >= 1";
+        }
         impl(const impl &) = delete;
         impl &operator =(const impl &) = delete;
 
-        // unbuffered channels use 0 capacity so send
-        // will block waiting for recv on empty channel
         size_type capacity;
-        size_type unread;
         std::queue<T, ContainerT> queue;
         qutex qtx;
         rendez not_empty;
         rendez not_full;
         bool closed;
 
-        bool is_empty() const { return unread == 0; }
-        bool is_full() const { return unread >= capacity; }
+        bool is_empty() const { return queue.empty(); }
+        bool is_full() const { return queue.size() >= capacity; }
 
         size_type send(T &&p) {
             std::unique_lock<qutex> lk(qtx);
-            size_type ret = unread;
+            bool synchronous = capacity == 1;
+            size_type ret = queue.size();
             while (is_full() && !closed) {
                 not_full.sleep(lk);
             }
             check_closed();
             queue.push(std::move(p));
-            ++unread;
             not_empty.wakeup();
+            if (synchronous) {
+                // wait for recv to complete
+                while (is_full() && !closed) {
+                    not_full.sleep(lk);
+                }
+            }
             return ret;
         }
 
         T recv() {
             std::unique_lock<qutex> lk(qtx);
-            bool unbuffered = capacity == 0;
-            if (unbuffered) {
-                // grow the capacity for a single item
-                capacity = 1;
-                // unblock sender
-                not_full.wakeup();
-            }
+            bool synchronous = capacity == 1;
             while (is_empty() && !closed) {
                 not_empty.sleep(lk);
             }
-            if (unread == 0) {
+            if (queue.empty()) {
                 check_closed();
             }
-
-            --unread;
             T item(std::move(queue.front()));
             queue.pop();
-            if (unbuffered) {
-                // shrink capacity again so sends will block
-                // waiting for recv
-                capacity = 0;
+            if (synchronous) {
+                // wake up all because some senders might
+                // we blocked waiting for recv to complete
+                // others might be blocked waiting to send
+                not_full.wakeupall();
             } else {
                 not_full.wakeup();
             }
