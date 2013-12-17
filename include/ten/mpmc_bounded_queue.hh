6c46da3cb5fd5cd6070a9d2e41e99907ef8706b3
Jason Toffaletti <jason@topsy.com>
2013-12-17 12:56:41 -0800

    Revert "attempt to fix case where queue position counters overflow."
    This reverts commit d965a312cb2d8e4d49ba190fcf310dfb8b0f5772.

--- include/ten/mpmc_bounded_queue.hh
+++ include/ten/mpmc_bounded_queue.hh
@@ -33,28 +33,25 @@
 
 namespace ten {
 
-template<typename T, std::size_t buffer_size_, typename SizeType = std::size_t>
+template<typename T, std::size_t buffer_size>
 class mpmc_bounded_queue
 {
-public:
-    typedef SizeType size_type;
-    static const std::size_t buffer_size = buffer_size_;
 private:
     struct cell_t {
-        std::atomic<size_type> sequence_;
-        T                      data_;
+        std::atomic<std::size_t> sequence_;
+        T                   data_;
     };
 
-    static std::size_t const  cacheline_size = 64;
-    typedef char              cacheline_pad_t [cacheline_size];
+    static std::size_t const     cacheline_size = 64;
+    typedef char            cacheline_pad_t [cacheline_size];
 
     cacheline_pad_t           pad0_;
     cell_t *const             buffer_;
-    size_type const           buffer_mask_ = buffer_size-1;
+    std::size_t const         buffer_mask_ = buffer_size-1;
     cacheline_pad_t           pad1_;
-    std::atomic<size_type>    enqueue_pos_;
+    std::atomic<std::size_t>  enqueue_pos_;
     cacheline_pad_t           pad2_;
-    std::atomic<size_type>    dequeue_pos_;
+    std::atomic<std::size_t>  dequeue_pos_;
     cacheline_pad_t           pad3_;
 
 public:
@@ -81,10 +78,10 @@ public:
 
     bool enqueue(T const& data) {
         cell_t* cell;
-        size_type pos = enqueue_pos_.load(std::memory_order_relaxed);
+        std::size_t pos = enqueue_pos_.load(std::memory_order_relaxed);
         for (;;) {
             cell = &buffer_[pos & buffer_mask_];
-            size_type seq = cell->sequence_.load(std::memory_order_acquire);
+            std::size_t seq = cell->sequence_.load(std::memory_order_acquire);
             intptr_t dif = (intptr_t)seq - (intptr_t)pos;
             if (dif == 0) {
                 if (enqueue_pos_.compare_exchange_weak(
@@ -92,10 +89,6 @@ public:
                     break;
                 }
             } else if (dif < 0) {
-                return false;
-            } else if (pos == 0 && (seq-1) % buffer_size == 0) {
-                // this handles the case where enqueue_pos has overflowed
-                // back to 0 and the queue is full
                 return false;
             } else {
                 pos = enqueue_pos_.load(std::memory_order_relaxed);
@@ -110,19 +103,12 @@ public:
 
     bool dequeue(T& data) {
         cell_t* cell;
-        size_type pos = dequeue_pos_.load(std::memory_order_relaxed);
+        std::size_t pos = dequeue_pos_.load(std::memory_order_relaxed);
         for (;;) {
             cell = &buffer_[pos & buffer_mask_];
-            size_type seq = cell->sequence_.load(std::memory_order_acquire);
+            std::size_t seq = cell->sequence_.load(std::memory_order_acquire);
             intptr_t dif = (intptr_t)seq - (intptr_t)(pos + 1);
             if (dif == 0) {
-                if (dequeue_pos_.compare_exchange_weak(
-                            pos, pos + 1, std::memory_order_relaxed)) {
-                    break;
-                }
-            } else if (seq == 0 && pos == std::numeric_limits<uint8_t>::max()) {
-                // catch the case where pos+1 would overflow back to 0 causing
-                // diff to be negative and dequeue to fail
                 if (dequeue_pos_.compare_exchange_weak(
                             pos, pos + 1, std::memory_order_relaxed)) {
                     break;
--- tests/test_mpmc_queue.cc
+++ tests/test_mpmc_queue.cc
@@ -6,30 +6,31 @@
 using namespace ten;
 using std::size_t;
 
-TEST(MPMCQueue, Test) {
-    typedef mpmc_bounded_queue<int, 128, uint8_t> queue_t;
-    static size_t const thread_count = 4;
-    static size_t const batch_size = 1;
-    static size_t const iter_count = 1000000;
+typedef mpmc_bounded_queue<int, 1024> queue_t;
 
-    queue_t queue;
+static size_t const thread_count = 4;
+static size_t const batch_size = 1;
+static size_t const iter_count = 1000000;
 
-    auto thread_func = [](queue_t &queue) {
-        int data;
+static void thread_func(queue_t &queue) {
+    int data;
 
-        for (size_t iter = 0; iter != iter_count; ++iter) {
-            for (size_t i = 0; i != batch_size; i += 1) {
-                while (!queue.enqueue(i)) {
-                    std::this_thread::yield();
-                }
-            }
-            for (size_t i = 0; i != batch_size; i += 1) {
-                while (!queue.dequeue(data)) {
-                    std::this_thread::yield();
-                }
+    for (size_t iter = 0; iter != iter_count; ++iter) {
+        for (size_t i = 0; i != batch_size; i += 1) {
+            while (!queue.enqueue(i)) {
+                std::this_thread::yield();
             }
         }
-    };
+        for (size_t i = 0; i != batch_size; i += 1) {
+            while (!queue.dequeue(data)) {
+                std::this_thread::yield();
+            }
+        }
+    }
+}
+
+TEST(MPMCQueue, Test) {
+    queue_t queue;
 
     std::array<std::thread, thread_count> threads;
     for (size_t i = 0; i != thread_count; ++i) {
@@ -40,77 +41,5 @@ TEST(MPMCQueue, Test) {
 
     for (size_t i = 0; i != thread_count; ++i) {
         threads[i].join();
-    }
-}
-
-TEST(MPMCQueue, QueueOverflow8) {
-    typedef mpmc_bounded_queue<int, 8, uint8_t> queue_t;
-    int i = 0;
-
-    queue_t queue;
-
-    for (size_t x=1; x<10; ++x) {
-        for (; i < 10000; ++i) {
-            if (!queue.enqueue(i)) break;
-        }
-
-        while (queue.dequeue(i)) {
-        }
-
-        ASSERT_EQ((x*queue_t::buffer_size)-x, i);
-    }
-}
-
-TEST(MPMCQueue, QueueOverflow64) {
-    typedef mpmc_bounded_queue<int, 64, uint8_t> queue_t;
-    int i = 0;
-
-    queue_t queue;
-
-    for (size_t x=1; x<10; ++x) {
-        for (; i < 10000; ++i) {
-            if (!queue.enqueue(i)) break;
-        }
-
-        while (queue.dequeue(i)) {
-        }
-
-        ASSERT_EQ((x*queue_t::buffer_size)-x, i);
-    }
-}
-
-TEST(MPMCQueue, QueueOverflow128) {
-    typedef mpmc_bounded_queue<int, 128, uint8_t> queue_t;
-    int i = 0;
-
-    queue_t queue;
-
-    for (size_t x=1; x<10; ++x) {
-        for (; i < 10000; ++i) {
-            if (!queue.enqueue(i)) break;
-        }
-
-        while (queue.dequeue(i)) {
-        }
-
-        ASSERT_EQ((x*queue_t::buffer_size)-x, i);
-    }
-}
-
-TEST(MPMCQueue, QueueOverflow256) {
-    typedef mpmc_bounded_queue<int, 256, uint8_t> queue_t;
-    int i = 0;
-
-    queue_t queue;
-
-    for (size_t x=1; x<10; ++x) {
-        for (; i < 10000; ++i) {
-            if (!queue.enqueue(i)) break;
-        }
-
-        while (queue.dequeue(i)) {
-        }
-
-        ASSERT_EQ((x*queue_t::buffer_size)-x, i);
     }
 }
